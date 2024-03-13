85b36392a9759a9d173ee0b3a8d056aab67819fe
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-03-13 18:36:41 +0000

    test

--- src/hw3.c
+++ src/hw3.c
@@ -9,8 +9,80 @@
 #define DEBUG(...) fprintf(stderr, "[          ] [ DEBUG ] "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, " -- %s()\n", __func__)
 
 GameState* initialize_game_state(const char *filename) {
-    (void)filename;
-    return NULL;
+    FILE *txtFile = fopen(filename, "r");
+    if (!txtFile) 
+    {
+        perror("Error opening file");
+        return NULL;
+    }
+
+    int totalRows = 0, maxColumn = 0, totalColumns = 0;
+    char ch;
+    while ((ch = fgetc(txtFile)) != EOF) 
+    {
+        totalColumns++;
+        if (ch == '\n') 
+        {
+            totalRows++;
+            if (totalColumns > maxColumn) 
+            {
+                maxColumn = totalColumns - 1; 
+            }
+            totalColumns = 0; 
+        }
+    }
+    if (totalColumns > 0) 
+    { 
+        totalRows++; 
+        if (totalColumns > maxColumn) 
+        {
+            maxColumn = totalColumns; 
+        }
+    }
+
+    fseek(txtFile, 0, SEEK_SET);
+
+    GameState *game = malloc(sizeof(GameState));
+    if (!game) 
+    {
+        perror("Malloc failed");
+        fclose(txtFile);
+        return NULL;
+    }
+
+    game->row = totalRows;
+    game->column = maxColumn;
+
+    game->grid = malloc(game->row * sizeof(char**));
+    for (int i = 0; i < game->row; ++i) 
+    {
+        game->grid[i] = malloc(game->column * sizeof(char*));
+        for (int j = 0; j < game->column; ++j) 
+        {
+            game->grid[i][j] = calloc(MAX_STACK_HEIGHT, sizeof(char));
+            strcpy(game->grid[i][j], ".");
+        }
+    }
+
+    int rows = 0, columns = 0;
+    while ((ch = fgetc(txtFile)) != EOF) 
+    {
+        if (ch == '\n') 
+        {
+            rows++;
+            columns = 0;
+        } else {
+            if (columns < maxColumn) 
+            {
+                game->grid[rows][columns][0] = ch;
+                game->grid[rows][columns][1] = '\0';
+                columns++;
+            }
+        }
+    }
+
+    fclose(txtFile);
+    return game;
 }
 
 GameState* place_tiles(GameState *game, int row, int col, char direction, const char *tiles, int *num_tiles_placed) {
